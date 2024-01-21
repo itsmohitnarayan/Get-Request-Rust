e3db13f53aa840bbd40a38c2bfedbf24e0af4668
Kumar Mohit <mohit724196@gmail.com>
2024-01-21 20:33:13 +0530

    code

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
-fn main() {
-    println!("Hello, world!");
+error_chain
+
+fn main() -> Result<()>{
+    let mut res = reqwest::blocking::get()?;
+    let mut body = string::new();
+    res.read_to_string(&mut body)?;
+
+    println!("status :{}", res.status());
+    println!("Header: \n{:#?}", res.Header());
+    println!("body: \n{}", body);
+    ok(())
 }
