f9579f74eda5ae2fb81a957f53995675ce1c51b6
Kumar Mohit <mohit724196@gmail.com>
2024-01-21 20:42:50 +0530

    changes

--- src/main.rs
+++ src/main.rs
@@ -4,17 +4,17 @@ use std::io::Read;
 error_chain! {
     foreign_links{
         Io(std::io::Error);
-        HttpRequest(Reqwest::Error);
+        HttpRequest(reqwest::Error);
     }
 }
 
 fn main() -> Result<()>{
-    let mut res = reqwest::blocking::get()?;
-    let mut body = string::new();
+    let mut res = reqwest::blocking::get("http://httpbin.org/get")?;
+    let mut body = String::new();
     res.read_to_string(&mut body)?;
 
     println!("status :{}", res.status());
-    println!("Header: \n{:#?}", res.Header());
+    println!("Header: \n{:#?}", res.headers());
     println!("body: \n{}", body);
-    ok(())
+    Ok(())
 }
